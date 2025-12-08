cc8ea8a3e886d16e4f6332748e7e9959b98b9462
Ren <liren12332@gmail.com>
2025-12-07 22:10:06 -0500

    add comments,js by Ren

---
+++ public/js/comments.js
@@ -0,0 +1,83 @@
+(function ($) {
+
+    function loadComments() {
+        var arrestId = $('#arrestId').val();
+
+        $.ajax({
+            method: "GET",
+            url: '/comments/arrest/' + arrestId,
+            dataType: "json"
+        }).then(function (data) {
+            var container = $('#comments-area');
+
+            if (!data.comments || data.comments.length === 0) {
+                container.html("<p>No comments yet. Be the first!</p>");
+                return;
+            }
+
+            var userId = $('#userId').val();
+
+            var html = "<ul class='comment-list'>";
+
+            data.comments.forEach(function (c) {
+                html += `
+                    <li class="comment-item">
+                        <strong>${c.username || "User"}:</strong>
+                        <p>${c.text}</p>
+                        <small>${new Date(c.createdAt).toLocaleString()}</small>
+                        ${String(c.userId) === userId ?
+                        `<button class="delete-btn" data-id="${c._id}">Delete</button>`
+                        : ""}
+                    </li>
+                `;
+            });
+
+            html += "</ul>";
+            container.html(html);
+        });
+    }
+
+    $('#comments-area').on('click', '.delete-btn', function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        var commentId = button.data('id');
+        var userId = $('#userId').val();
+
+        $.ajax({
+            method: "DELETE",
+            url: '/comments/' + commentId,
+            contentType: 'application/json',
+            data: JSON.stringify({ userId: userId })
+        }).then(function () {
+            loadComments();
+        });
+    });
+
+    $('#comment-form').on('submit', function (event) {
+        event.preventDefault();
+
+        var arrestId = $('#arrestId').val();
+        var userId = $('#userId').val();
+        var content = $('#comment-text').val();
+
+        $.ajax({
+            method: "POST",
+            url: '/comments',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                arrestId: arrestId,
+                userId: userId,
+                content: content
+            })
+        }).then(function () {
+            $('#comment-text').val("");
+            loadComments();
+        });
+    });
+
+    $(document).ready(function () {
+        loadComments();
+    });
+
+})(jQuery);
